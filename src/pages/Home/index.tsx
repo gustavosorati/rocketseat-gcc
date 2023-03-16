128265e6018f74b13ef4fb048b7ce4d36d8f47cd
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-16 16:58:24 -0300

    update organization of function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,100 @@
-import { Container, Content, Left, Right, SelectStateFilter } from './styles'
+import { Container, Content, Left, Right } from './styles'
 import logoSvg from '../../assets/icons/logo.svg'
-import { SelectState } from '@/components/SelectState'
-import { Select } from '@/components/Select'
-import { Aside } from '@/components/Aside'
 import { Button } from '@/components/Button'
+import { FormEvent, useEffect, useState } from 'react'
+import { api } from '@/utils/api'
+import { IStateDTO, IStatesRequest } from '@/interfaces/State'
+import { Select } from '@/components/Select'
+import { ICitiesDTO, ICitysRequest } from '@/interfaces/Cities'
 
 export function Home() {
-  function handleSearchPets() {
-    // TO DO
+  const [states, setStates] = useState<IStateDTO[]>([])
+  const [cities, setCities] = useState<ICitiesDTO[]>([])
+
+  const [state, setState] = useState('')
+  const [city, setCity] = useState('')
+
+  // const [pets, setPets] = useState<[]>([])
+
+  async function getStates() {
+    console.log('GET STATES =>')
+    try {
+      const response = await api.get<IStatesRequest>('/location/states')
+
+      const treatedStatesResponse = response.data.states.map((state) => {
+        return {
+          label: state.sigla,
+          value: state.sigla,
+        }
+      })
+
+      setStates(treatedStatesResponse)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  function handleChangeState() {
-    // TO DO
+  async function getCities(stateUF: string) {
+    try {
+      const response = await api.get<ICitysRequest>(
+        `/location/citys/${stateUF}`,
+      )
+
+      const treatedCitiesResponse = response.data.citys.map((city) => {
+        return {
+          label: city.name,
+          value: city.name,
+        }
+      })
+
+      setCities(treatedCitiesResponse)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  function handleChangeCity() {
-    // TO DO
+  async function handleChangeState(stateUF: string) {
+    setState(stateUF)
+
+    await getCities(stateUF)
   }
+
+  function handleChangeCity(city: string) {
+    setCity(city)
+  }
+
+  async function handleSearchPets() {
+    if (!state || !city) return
+
+    try {
+      console.log(city)
+      const response = await api.get(`/pets/${city}`)
+
+      console.log(response)
+      // const treatedCitiesResponse = response.data.states.map(
+      //   (state: IStateDTO) => {
+      //     return {
+      //       label: state.sigla,
+      //       value: state.sigla,
+      //     }
+      //   },
+      // )
+
+      // setStates(treatedCitiesResponse)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    await handleSearchPets()
+  }
+
+  useEffect(() => {
+    getStates()
+  }, [])
 
   return (
     <Container>
@@ -40,12 +118,26 @@ export function Home() {
         <Right>
           <img className="dogs" src="/dogs.png" alt="" />
 
-          <form>
-            <SelectState label="Busque um amigo:" name="state" options={[]} />
+          <form onSubmit={handleSubmit}>
+            <Select
+              name="state"
+              label="Busque um amigo:"
+              defaultText="UF"
+              options={states}
+              variant="tertiary"
+              update={handleChangeState}
+            />
 
-            <Select label="" name="city" options={[]} variant="secondary" />
+            <Select
+              label=""
+              name="state"
+              defaultText="Escolha a cidade"
+              options={cities}
+              variant="secondary"
+              update={handleChangeCity}
+            />
 
-            <Button />
+            <Button disabled={!state || !city} />
           </form>
         </Right>
       </Content>
