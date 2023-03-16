0f287a6f389643203b584c49e9a0580eb227f83c
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-16 11:55:16 -0300

    feat: create interface home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,9 @@
-import { Container } from './styles'
+import { Container, Content, Left, Right, SelectStateFilter } from './styles'
+import logoSvg from '../../assets/icons/logo.svg'
+import { SelectState } from '@/components/SelectState'
+import { Select } from '@/components/Select'
+import { Aside } from '@/components/Aside'
+import { Button } from '@/components/Button'
 
 export function Home() {
   function handleSearchPets() {
@@ -13,5 +18,37 @@ export function Home() {
     // TO DO
   }
 
-  return <Container></Container>
+  return (
+    <Container>
+      <Content>
+        <Left>
+          <header>
+            <img
+              src={logoSvg}
+              alt="Imagem de da caricatura do rosto de um cachorro"
+            />
+            <h3>FindAFriend</h3>
+          </header>
+
+          <h1>
+            Leve <br /> a felicidade <br /> para o seu lar
+          </h1>
+
+          <p>Encontre o animal de estimação ideal para seu estilo de vida!</p>
+        </Left>
+
+        <Right>
+          <img className="dogs" src="/dogs.png" alt="" />
+
+          <form>
+            <SelectState label="Busque um amigo:" name="state" options={[]} />
+
+            <Select label="" name="city" options={[]} variant="secondary" />
+
+            <Button />
+          </form>
+        </Right>
+      </Content>
+    </Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,68 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors['red-200']};
+  width: 100%;
+  height: 100vh;
+  padding: 6.87rem; // 110px
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Content = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  min-height: 580px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 7rem; // 110px
+`
+
+export const Left = styled.div`
+  width: 480px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 580px;
+
+  header {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem; // 12px
+
+    h3 {
+      font-size: 1.75rem; // 28px
+    }
+  }
+
+  h1 {
+    font-size: 4.5rem; // 72px
+  }
+
+  p {
+    font-size: 1.5rem; // 24px
+  }
+`
+
+export const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 580px;
+
+  img.dogs {
+    max-width: 592px;
+    width: 100%;
+    margin-top: 6rem;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+  }
+`
