2fb8e4294f3cddeef75e09c6acd1953c0f43b195
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-16 16:56:39 -0300

    update component with variants styles

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,5 +1,5 @@
 import chevron from '@/assets/icons/chevron-bottom.svg'
-import { ComponentProps } from 'react'
+import { ChangeEvent, ComponentProps } from 'react'
 import {
   Filter,
   FilterLabel,
@@ -11,31 +11,44 @@ import {
 type SelectProps = ComponentProps<typeof FilterInput> & {
   label: string
   name: string
+  defaultText: string
   options: {
     value: string | number
     label: string
   }[]
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary' | 'tertiary'
+  update: (value: string) => void
 }
 
 export function Select({
   label,
   name,
-  variant = 'primary',
+  defaultText,
   options,
+  variant = 'primary',
+  update,
 }: SelectProps) {
+  async function handleUpdate(event: ChangeEvent<HTMLSelectElement>) {
+    update(event.target.value)
+  }
+
   return (
-    <Filter>
+    <Filter variant={variant}>
       {label && <FilterLabel htmlFor={name}>{label}</FilterLabel>}
-      <FilterWrapper>
-        <FilterInput name={name} id={name} variant={variant}>
-          <FilterInputOption value="" disabled selected>
-            Selecione dfsdfsdf
+      <FilterWrapper variant={variant}>
+        <FilterInput
+          name={name}
+          id={name}
+          variant={variant}
+          onChange={handleUpdate}
+        >
+          <FilterInputOption value="" defaultValue="">
+            {defaultText}
           </FilterInputOption>
           {options.map((option) => {
             return (
               <FilterInputOption key={option.value} value={option.value}>
-                {option.label}
+                {option.value}
               </FilterInputOption>
             )
           })}
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -1,10 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Filter = styled.div`
+interface FilterProps {
+  variant?: 'primary' | 'secondary' | 'tertiary'
+}
+
+export const Filter = styled.div<FilterProps>`
   display: flex;
   flex-direction: column;
   gap: 12px;
-  width: 280px;
+
+  ${({ variant }) =>
+    variant === 'tertiary' &&
+    css`
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+    `}
 `
 
 export const FilterLabel = styled.label`
@@ -13,7 +24,11 @@ export const FilterLabel = styled.label`
   font-weight: 500;
 `
 
-export const FilterWrapper = styled.div`
+interface SelectProps {
+  variant?: 'primary' | 'secondary' | 'tertiary'
+}
+
+export const FilterWrapper = styled.div<SelectProps>`
   position: relative;
 
   & > img {
@@ -21,28 +36,52 @@ export const FilterWrapper = styled.div`
     right: 18px;
     top: 50%;
     transform: translateY(-50%);
+
+    ${({ variant }) =>
+      variant === 'tertiary' &&
+      css`
+        width: 10px;
+        left: 40px;
+      `}
   }
 `
-
-interface FilterInputProps {
-  variant?: 'primary' | 'secondary'
-}
-
-export const FilterInput = styled.select<FilterInputProps>`
+export const FilterInput = styled.select<SelectProps>`
   width: 100%;
   height: 60px;
   font-size: 16px;
   line-height: 19.2px;
   font-weight: 800;
   color: #ffffff;
-  background-color: ${({ variant, theme }) =>
-    variant === 'primary' ? theme.colors['red-200'] : theme.colors['red-400']};
+
   border-radius: 15px;
   border: none;
   outline: none;
   padding: 20px;
   appearance: none;
   position: relative;
+
+  ${({ variant, theme }) =>
+    variant === 'primary' &&
+    css`
+      background-color: ${theme.colors['red-200']};
+      min-width: 280px;
+    `}
+
+  ${({ variant, theme }) =>
+    variant === 'secondary' &&
+    css`
+      background-color: ${theme.colors['red-400']};
+      min-width: 280px;
+    `}
+
+  ${({ variant, theme }) =>
+    variant === 'tertiary' &&
+    css`
+      background-color: ${theme.colors['red-200']};
+      border: 1px solid ${theme.colors.white};
+      min-width: 60px;
+      padding: 10px;
+    `}
 
   &::before {
     content: '⌄';
